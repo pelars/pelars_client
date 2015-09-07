b624fe9289eab57f4d7df0414b9c8691fdfd5f0a
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2015-09-07 15:55:48 +0200

    new build and fix

--- src/session_manager.cpp
+++ src/session_manager.cpp
@@ -5,7 +5,7 @@ SessionManager::SessionManager(std::string endpoint): endpoint_(endpoint)
 {	
 	//srand (time(NULL));
 	std::cout << "Parsing the input data" << std::endl;
-    tinyxml2::XMLError eResult =  data_.LoadFile( "../../data/personal.xml" );
+   auto eResult =  data_.LoadFile( "../../data/personal.xml" );
     if(eResult != 0){
     	std::cout << "\tError parsing personal.xml or file not present in /data/" << std::endl;
     	to_stop = true;
