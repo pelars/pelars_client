f4243f570872ff634892b0f355fa5eb37bc6da7d
Giacomo Dabisias <g.dabisias@sssup.it>
2016-06-01 14:11:54 +0200

    adds wav audio

--- src/audio_recorder.cpp
+++ src/audio_recorder.cpp
@@ -22,13 +22,15 @@ void audioRecorder(unsigned int session){
 		}
 
 		
-		std::string device = exec("pactl list | grep -A2 'Source #' | grep 'Name: ' | cut -d\" \" -f2 | grep C920");
+		//std::string device = exec("pactl list | grep -A2 'Source #' | grep 'Name: ' | cut -d\" \" -f2 | grep C920");
 
-		std::cout << "Recording audio from " << device << std::endl;
+		//std::cout << "Recording audio from " << device << std::endl;
 
 		std::stringstream ss;
 
-		ss << "gst-launch-1.0 pulsesrc device=\"" << device << "\" ! audio/x-raw,channels=2 ! avenc_ac3 bitrate=192000 ! filesink location=" << image_folder_name << "/"<< session << ".ac3 &";
+		//ss << "gst-launch-1.0 pulsesrc device=\"" << device << "\" ! audio/x-raw,channels=2 ! avenc_ac3 bitrate=192000 ! filesink location=" << image_folder_name << "/"<< session << ".ac3 &";
+		ss << "gst-launch-1.0 alsasrc device=sysdefault:CARD=C920 ! audioconvert ! audioresample ! wavenc ! filesink location=" << image_folder_name << "/"<< session << ".wav &";
+
 		std::string execute = ss.str();
 		std::cout << "Executing " << execute << std::endl;
 		exec(execute.c_str());
